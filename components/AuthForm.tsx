addf6457c1ccb721bb597a6faa71a6bd67f49a4b
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2024-05-07 16:22:00 +0000

    Setting up Appwrite and User Auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -39,16 +39,16 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plaid token
 
       if (type === "sign-up") {
-        //   const newUser = await signUp(data);
-        //   setUser(newUser);
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
 
       if (type === "sign-in") {
-        //   const response = await signIn({
-        //     email: data.email,
-        //     password: data.password,
-        //   })
-        //   if(response) router.push('/')
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
